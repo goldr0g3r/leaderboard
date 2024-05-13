92452f076cee4082c602a60751eb8f9d5712f165
OkBeiRohan <rohithkamal@gmail.com>
2024-05-13 17:46:55 +0530

    logger

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,5 +1,14 @@
 import express, { Express, Request, Response } from "express";
+import { connectDB } from "./config/mongodb";
+import helmet from "helmet";
+import morgan from "morgan";
 
 const app: Express = express();
+
+connectDB();
+
+app.use(helmet());
+app.use(express.json());
+app.use(morgan("dev"));
 
 export default app;
